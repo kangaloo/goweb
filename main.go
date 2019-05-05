56fcd95dac53b017b2dc4c98cc5c9b32f93fe3bc
Xiangyang Li <13622004447@163.com>
2019-05-05 14:20:58 +0800

    simple http server

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("hello world"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
