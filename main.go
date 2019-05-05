3dbfbb2fc2a1aa0ee02eb96aef488c5f396f1902
Xiangyang Li <13622004447@163.com>
2019-05-05 15:07:04 +0800

    simple template server

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type User struct {
+	Username string
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("hello world"))
+		user := &User{
+			Username: "kangaroo",
+		}
+
+		tpl, err := template.New("index.html").ParseFiles("templates/index.html")
+		if err != nil {
+			log.Fatalln("parse error", err)
+		}
+
+		/*
+			https://stackoverflow.com/questions/49043292/error-template-is-an-incomplete-or-empty-template
+			2019/05/05 15:02:09 exec error template: "welcome" is an incomplete or empty template
+		*/
+		if err := tpl.Execute(w, user); err != nil {
+			log.Fatalln("exec error", err)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
