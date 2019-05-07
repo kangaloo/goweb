68d8171202cf61de0d861e4b16e81c0b486bc799
Xiangyang Li <13622004447@163.com>
2019-05-07 21:17:45 +0800

    simple template server

--- main.go
+++ main.go
@@ -10,25 +10,31 @@ type User struct {
 	Username string
 }
 
+type IndexViewModel struct {
+	Title string
+	User  User
+}
+
+func IndexHandle(w http.ResponseWriter, r *http.Request) {
+	user := &User{
+		Username: "kangaroo",
+	}
+
+	tpl, err := template.New("index.html").ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatalf("parse files error: %v", err)
+	}
+	/*
+		https://stackoverflow.com/questions/49043292/error-template-is-an-incomplete-or-empty-template
+		2019/05/05 15:02:09 exec error template: "welcome" is an incomplete or empty template
+	*/
+
+	if err := tpl.Execute(w, user); err != nil {
+		log.Fatalf("exec template error: %v", err)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		user := &User{
-			Username: "kangaroo",
-		}
-
-		tpl, err := template.New("index.html").ParseFiles("templates/index.html")
-		if err != nil {
-			log.Fatalln("parse error", err)
-		}
-
-		/*
-			https://stackoverflow.com/questions/49043292/error-template-is-an-incomplete-or-empty-template
-			2019/05/05 15:02:09 exec error template: "welcome" is an incomplete or empty template
-		*/
-		if err := tpl.Execute(w, user); err != nil {
-			log.Fatalln("exec error", err)
-		}
-	})
-
+	http.HandleFunc("/", IndexHandle)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
